d80472ffe019ed09687d5e2f7dfed0cafc5a4e50
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2020-09-24 18:33:37 -0400

    added answer buttons and answer texts under the quiz question

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var interval;
 var time = 100;
 var scores = ["JG - 22"];
 var questionWrong = false;
+var quizIndex = 0;
 var quiz = [
     {
         question: "Which of the following are valid JavaScript data types?",
@@ -53,7 +54,8 @@ var quiz = [
     },
 
 ];
-// console.log(quiz[1].answers[2]);
+console.log(quiz[0].question);
+console.log(quiz[1].answers[2]);
 
 var sc
 //function for timer (start quiz)
@@ -78,21 +80,33 @@ function startQuiz() {
         }
     }, 1000);
 
-    var i = 0;
+    
     while (time > 0) {
-        var q = quiz[i].question;
-        secondRow.textContent = q.question;
+        secondRow.textContent = quiz[quizIndex].question;
         var ol = document.createElement("ol");
             //not sure if this for loop will work or if i can use quiz[i].question.answers
-        for (var j = 0; j < quiz[i].answers.length; j++) {
+        for (var j = 0; j < quiz[quizIndex].answers.length; j++) {
+            // changing start button to the first answer 
+            if (j == 0) {
+                var li = document.createElement("li");
+                start.textContent = quiz[quizIndex].answers[j];
+                li.append(start);
+                start.setAttribute("index", j);
+                ol.appendChild(li);
+                continue;
+            }
             var li = document.createElement("li");
             var btn = document.createElement("button");
-            btn.textContent = quiz[i].answers[j];
+
+            btn.setAttribute("index", j);
+            btn.textContent = quiz[quizIndex].answers[j];
+
             li.appendChild(btn);
             ol.appendChild(li);
         }
-        btnEls.addEventListener("click", nextQuestion);
-        i++;
+        buttons.appendChild(ol);
+        btnEls.addEventListener("click", nextQuestion(quizIndex));
+    
     }   
 }
 
@@ -120,12 +134,13 @@ function viewScores() {
 //   that decrements the time if question is incorrect
 //   update score to local storage
 
-function nextQuestion() {
+function nextQuestion(event) {
     if(event.target.matches("button")) {
         //check the data-index attribute of the button and compare
         //with the index of the answer
+        quizIndex++;
     }
-})
+};
 
 
 start.addEventListener("click", startQuiz);
