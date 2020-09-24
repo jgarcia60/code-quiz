d00c05368e9d516eac0e2d1b819a62582d515070
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2020-09-24 09:20:34 -0400

    set up outline for javascript and html

---
+++ script.js
@@ -0,0 +1,13 @@
+//define all variables
+//including user inputs
+
+
+//function for timer (start quiz)
+//if timer runs out, quiz is over
+//if all questions are submitted, quiz is over
+//save score
+
+//function for event delegation (matches button)
+//   that decrements the time if question is incorrect
+//   update score to local storage
+
