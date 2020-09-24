6fde8c23d7e467bb54ddd068b97c90b77764ce5e
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2020-09-24 16:24:03 -0400

    added ids for DOM manipulation and established timer function

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //define all variables
 //including user inputs
+var highScores = document.getElementById("highScores");
+var start = document.getElementById("startButton");
+var interval;
+var time = 100;
+var questionWrong = false;
 
 
 //function for timer (start quiz)
@@ -7,8 +12,30 @@
 //if all questions are submitted, quiz is over
 //save score
 
+function startTimer() {
+    if (time == 0) {
+        //game over
+    }
+    interval = setInterval(function(){
+        time--;
+        document.getElementById("time").textContent = "Time: " + time;
+        if (questionWrong) {
+            if (time >= 10) {
+                //game over
+            } else {
+                time -= 10;
+            }
+        }
+    }, 1000);
+}
+
+function viewScores() {
+    console.log("You clicked the scores");
+}
+
 //function for event delegation (matches button)
 //   that decrements the time if question is incorrect
 //   update score to local storage
 
-console.log("hello world");
+start.addEventListener("click", startTimer);
+highScores.addEventListener("click", viewScores);
